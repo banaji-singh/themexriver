d298175b936d24cb62fae5c77071316b02190ed4
Rohit Singh <rohitsinghrathore529@gmail.com>
2025-08-06 19:02:41 +0530

    Add all 6 front-page sections and update styles

--- functions.php
+++ functions.php
@@ -7,5 +7,15 @@ function my_enqueue_style(){
 } 
 Add_action('wp_enqueue_scripts','my_enqueue_style');
 
+function load_front_page_styles() {
+    wp_enqueue_style('hero-style', get_template_directory_uri() . '/css/front-page/section1.css');
+    wp_enqueue_style('about-style', get_template_directory_uri() . '/css/front-page/section2.css');
+    wp_enqueue_style('services-style', get_template_directory_uri() . '/css/front-page/section3.css');
+    wp_enqueue_style('portfolio-style', get_template_directory_uri() . '/css/front-page/section4.css');
+    wp_enqueue_style('testimonials-style', get_template_directory_uri() . '/css/front-page/section5.css');
+    wp_enqueue_style('contact-style', get_template_directory_uri() . '/css/front-page/section6.css');
+}
+add_action('wp_enqueue_scripts', 'load_front_page_styles');
+
 
 ?>
