6fb3b05133b595f1fdf3d3487e9b9618662c7212
Rohit Singh <rohitsinghrathore529@gmail.com>
2025-08-14 13:18:35 +0530

    changes all updates and make it new

---
+++ template-parts/about-page/section4.php
@@ -0,0 +1,18 @@
+<section class="container-fluid mt-5 ">
+    <div class="row">
+        <div class=" col-lg-6 col-12 p-0 bg-gridient-l ">
+            <div class="s4-bg-img img-fluid w-100 p-5">
+                <div class="">
+                    <h1 class="theme-color fw-bold ">Managing your IT so you can focus on your business.</h1>
+                    <p class="mt-5 mb-5 ">Stratosphere Networks helps businesses bypass the traditional sales
+                    process and quickly identify leading Unified into Stratosphere Networks
+                    helps businesses bypass the traditional.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-0 p-0 bg-gridient-r">
+            <img src="https://themexriver.com/wp/technox/wp-content/uploads/2025/06/cta-2-img-1.webp" class="img-fluid w-100"style="height:70vh;" alt="">
+        </div>
+    </div>
+</section>
